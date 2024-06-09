b0a5d8048254a5eb81921274a0627ea8fc790ea7
Madhan-2004 <138513154+Madhan-2004@users.noreply.github.com>
2024-06-09 09:41:55 +0530

    new updates

--- XOX_project/Single_player.py
+++ XOX_project/Single_player.py
@@ -1,6 +1,7 @@
-from XOX import TicTacToe
 import random as rn
 import time as tm
+
+from XOX_project.Base_page import TicTacToe, initial_board
 
 
 class SinglePlayer(TicTacToe):
@@ -9,7 +10,7 @@ class SinglePlayer(TicTacToe):
         self.difficulty = difficulty
 
     def easy_move(self, symbol):
-        tm.sleep(1.2)
+        tm.sleep(1.1)
         move = rn.choice(list(self.available_positions))
         self.available_positions.remove(move)
         self.repeated_positions.add(move)
@@ -20,7 +21,7 @@ class SinglePlayer(TicTacToe):
             exit()
 
     def hard_move(self, symbol):
-        tm.sleep(1.2)
+        tm.sleep(0.8)
         move = self.calculate_best_move(symbol)
         self.available_positions.remove(move)
         self.repeated_positions.add(move)
@@ -31,17 +32,40 @@ class SinglePlayer(TicTacToe):
             exit()
 
     def calculate_best_move(self, symbol):
+        opponent_symbol = 'O' if symbol == 'X' else 'X'
+
+        # Check if we can win in the next move
         for pos in self.available_positions:
             self.board[pos - 1] = symbol
             if self.is_winner(symbol):
                 self.board[pos - 1] = " "
                 return pos
             self.board[pos - 1] = " "
+
+        # Check if the opponent can win in their next move, and block them
+        for pos in self.available_positions:
+            self.board[pos - 1] = opponent_symbol
+            if self.is_winner(opponent_symbol):
+                self.board[pos - 1] = " "
+                return pos
+            self.board[pos - 1] = " "
+
+        # Try to take the center
+        if 5 in self.available_positions:
+            return 5
+
+        # Try to take one of the corners
+        for pos in [1, 3, 7, 9]:
+            if pos in self.available_positions:
+                return pos
+
+        # Take any available position (shouldn't reach this in a properly played game)
         return rn.choice(list(self.available_positions))
+
 
     def play(self):
         print("Let's play Tic Tac Toe\nThis is the position...")
-        self.initial_board()
+        initial_board()
         print("\nand -1 for surrender...\nSo Choose your side...")
         start = input("X or O: ").upper()
         while start not in ["X", "O"]:
