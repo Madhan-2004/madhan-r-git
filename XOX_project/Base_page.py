bcd00ae22f8e255a583b14aa06c56838ae6f38d3
Madhan-2004 <138513154+Madhan-2004@users.noreply.github.com>
2024-08-26 01:10:59 +0530

    added surrender

--- XOX_project/Base_page.py
+++ XOX_project/Base_page.py
@@ -28,6 +28,10 @@ class TicTacToe:
         while True:
             try:
                 position = int(input("Enter position: "))
+                if position == -1:
+                    p2='X' if symbol=="O" else "O"
+                    print(f"\n{symbol} surrenders\n{p2} WON!")
+                    exit()
                 if position in self.available_positions:
                     self.available_positions.remove(position)
                     self.repeated_positions.add(position)
